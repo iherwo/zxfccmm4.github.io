ab6f40a842300bd0149ca21c0eeaf560658ace0c
Steve Zhou <zxfccmm@126.com>
2023-02-22 13:43:52 +0800

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,7 @@ export async function get() {
   return rss({
     title: "Austin's Blog",
     description: "Site description",
-    site: 'https://astro-blog.qum.cc',
+    site: 'https://blog.steveee.me',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>zh-cn</language>`,
   });
