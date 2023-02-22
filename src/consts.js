f1b80c68f08453aaf07d89a623d5fc26b9393308
Steve Zhou <zxfccmm@126.com>
2023-02-22 10:59:05 +0800

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Austin's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro-blog.qum.cc';
-export const SITE_URL = "https://astro-blog.qum.cc";
+export const SITE_TITLE = `Steve's Blog`;
+export const SITE_DESCRIPTION = 'Steve Site Description';
+export const SITE_EMAIL = 'zxfccmm@gmail.com'
+export const SITE_NAME = 'blog.steveee.me';
+export const SITE_URL = "https://blog.steveee.me";
