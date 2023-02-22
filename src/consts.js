22d545533cfecbedada88e64d349bf26ac7e4782
Steve Zhou <zxfccmm@126.com>
2023-02-22 14:25:28 +0800

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `Steve's Blog`;
 export const SITE_DESCRIPTION = 'Steve Site Description';
 export const SITE_EMAIL = 'zxfccmm@gmail.com'
-export const SITE_NAME = 'blog.steveee.me';
+export const SITE_NAME = 'Steveee';
 export const SITE_URL = "https://blog.steveee.me";
